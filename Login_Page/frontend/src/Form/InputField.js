5ccbb80756c8fa6d16bbf73972f0ce4aa5dc6c08
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 10:49:51 +0530

    created component for input fields

---
+++ Login_Page/frontend/src/Form/InputField.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class InputField extends React.Component {
+
+  render() {
+  return (
+    <div className="inputField">
+      Hello
+    </div>
+  );
+  }
+}
+
+export default InputField;
