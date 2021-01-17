6502b7b863eb34fea4e1ed9366eeaaf0091d7bf8
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 10:48:26 +0530

    created form component

---
+++ Login_Page/frontend/src/Form/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+
+  render() {
+  return (
+    <div className="loginForm">
+      Hello
+    </div>
+  );
+  }
+}
+
+export default LoginForm;
