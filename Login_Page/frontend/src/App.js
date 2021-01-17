1ebc6723a00a9c02a0521d0199f6779eca1e87c3
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 10:53:49 +0530

    imported form components

--- Login_Page/frontend/src/App.js
+++ Login_Page/frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import UserStore from './store/UserStore'
+import LoginForm from './Form/LoginForm'
+import InputField from './Form/InputField'
+import SubmitButton from './Form/SubmitButton'
 import './App.css';
 
 class App extends React.Component {
@@ -8,6 +11,9 @@ class App extends React.Component {
   return (
     <div className="">
       Hello
+      <LoginForm />
+      <InputField />
+      <SubmitButton />
     </div>
   );
   }
