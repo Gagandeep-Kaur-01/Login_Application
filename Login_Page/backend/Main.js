8f8d4c5b25d2eda27203f43fa542273c3bc27068
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 11:47:35 +0530

    database connectivity

--- Login_Page/backend/Main.js
+++ Login_Page/backend/Main.js
@@ -9,7 +9,45 @@ const MySQLStore = require('express-mysql-session')(session);
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 
-console.log('Testing server');
+//console.log('Testing server');
 
+//Database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'loginapp'
+});
+
+db.connect(function(err) {
+    if (err) {
+      console.log('DB error');
+      throw err;
+      return false;
+    }     
+}); 
+
+const sessionStore = new MySQLStore({
+    expiration: (1825 * 86400 * 1000),
+    endConnectionOnClose: false
+}, db);
+
+app.use(session({
+    key: 'rdfchjg87675gfvjhvhj',
+    secret: 'vgftgfc6gvjvhjb',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1825 * 86400 * 1000),
+        httpOnly: false
+    }
+}));
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(3000);
