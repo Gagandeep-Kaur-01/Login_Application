1f57424d64baa9d7a71e28d64ebbbb3e8f23b8d6
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 11:56:46 +0530

    created route for logOut

--- Login_Page/backend/Router.js
+++ Login_Page/backend/Router.js
@@ -78,6 +78,24 @@ class Router {
 
     logout(app, db) {
 
+        app.post('/logout', (req, res) => {
+            if(req.session.userID) {
+                req.session.destroy();
+                res.json({
+                    success: true
+                })
+
+                return true;
+
+            } else {
+                res.json({
+                    success: false
+                })
+
+                return false;
+            }
+        });
+
     }
 
     isLoggedIn(app, db) {
