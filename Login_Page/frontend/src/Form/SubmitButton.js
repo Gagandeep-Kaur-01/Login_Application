d98ab2876be6961f5e47b7677842ee76997bea3a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 10:52:00 +0530

    created component for submit button

---
+++ Login_Page/frontend/src/Form/SubmitButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SubmitButton extends React.Component {
+
+  render() {
+  return (
+    <div className="submitButton">
+      Hello
+    </div>
+  );
+  }
+}
+
+export default SubmitButton;
