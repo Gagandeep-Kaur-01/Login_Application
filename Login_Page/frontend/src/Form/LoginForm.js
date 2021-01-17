8487818db7001cbcc8814fd85ce1845cc223251a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-17 11:07:26 +0530

    started to create login form component

--- Login_Page/frontend/src/Form/LoginForm.js
+++ Login_Page/frontend/src/Form/LoginForm.js
@@ -1,11 +1,109 @@
 import React from 'react';
+import InputField from './InputField';
+import SubmitButton from './SubmitButton';
+import UserStore from '../store/UserStore';
 
 class LoginForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      buttonDisabled: false
+    }
+  }
+
+  setInputValue(property, val) {
+    val = val.trim();
+    if(val.length > 12) {
+      return;
+    }
+    this.setState({
+      [property]: val
+    })
+  }
+
+  resetForm() {
+    this.setState({
+      username: '',
+      password: '',
+      buttonDisabled: false
+    })
+  }
+
+  async doLogin() {
+
+    if (!this.state.username) {
+      return;
+    }
+    if (!this.state.password) {
+      return;
+    }
+
+    this.setState({
+      buttonDisabled: true      
+    })
+
+    try {
+
+      let res = await fetch('/login', {
+        method: 'post',
+        headers: {
+          'Accept' : 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        })
+      });
+
+      let result = await res.json();
+      if(result && result.success) {
+        UserStore.isLoggedIn = true;
+        UserStore.username = result.username;
+      }
+
+      else if (result && result.success === false) {
+        this.resetForm();
+        alert(result.mgs);
+      }
+
+    }
+
+    catch(e) {
+        console.log(e);
+        this.resetForm();
+    }
+
+  }
 
   render() {
   return (
     <div className="loginForm">
-      Hello
+
+      Log in 
+      <InputField
+        type='text'
+        placeholder='Username'
+        value={this.state.username ? this.state.username : ''}
+        onChange= { (val) => this.setInputValue('username', val) }
+      /> 
+
+      <InputField
+        type='password'
+        placeholder='Password'
+        value={this.state.password ? this.state.password : ''}
+        onChange= { (val) => this.setInputValue('password', val) }
+      />       
+
+      <SubmitButton
+          text='Login'
+          disabled={this.state.buttonDisabled}
+          onClick={ () => this.doLogin()}
+      /> 
+         
     </div>
   );
   }
